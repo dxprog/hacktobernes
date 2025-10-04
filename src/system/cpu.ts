9802a782c6b478cf8366f37eaf2f0a95cd98ee40
Matt Hackmann <matt.hackmann@gmail.com>
2025-10-03 21:36:32 -0700

    made to the NMI loop

--- src/system/cpu.ts
+++ src/system/cpu.ts
@@ -1,4 +1,4 @@
-import { MAX_ADDRESS } from '../common/constants';
+import { MAX_ADDRESS, MAX_BYTE } from '../common/constants';
 import { Bus } from './bus';
 
 const NMI_VECTOR_ADDR = 0xfffa;
@@ -25,11 +25,36 @@ type ProcessorFlag = (
 export class Cpu {
   private bus: Bus;
   // accumulator
-  private regA: number;
+  private _regA: number;
+  private set regA(value: number) {
+    this._regA = value;
+    this.setRegisterFlags(value);
+  }
+  private get regA(): number {
+    return this._regA;
+  }
+
   // X register
-  private regX: number;
+  private _regX: number;
+  private set regX(value: number) {
+    this._regX = value;
+    this.setRegisterFlags(value);
+  }
+  private get regX(): number {
+    return this._regX;
+  }
+
   // Y register
-  private regY: number;
+  private _regY: number;
+  private set regY(value: number) {
+    this._regY = value;
+    this.setRegisterFlags(value);
+  }
+  private get regY(): number {
+    return this._regY;
+  }
+
+
   // program counter
   private regPC: number;
   // status word
@@ -50,22 +75,33 @@ export class Cpu {
     this.opCodeMap = {
       [0x78]: () => this.setFlag(SW_FLAG_INT_DISABLE, 1),
       [0xD8]: () => this.setFlag(SW_FLAG_BCD_ENABLE, 0),
+
       // LDA
-      [0xA9]: () => this.lda(this.addrImmediate()),
-      [0xA5]: () => this.lda(this.read(this.addrZeroPage())),
-      [0xAD]: () => this.lda(this.read(this.addrAbsolute())),
+      [0xA9]: () => this.regA = this.addrImmediate(),
+      [0xA5]: () => this.regA = this.read(this.addrZeroPage()),
+      [0xAD]: () => this.regA = this.read(this.addrAbsolute()),
+
+      // LDY
+      [0xAC]: () => this.regY = this.read(this.addrAbsolute()),
 
       // STA
       [0X85]: () => this.sta(this.addrZeroPage()),
       [0x8D]: () => this.sta(this.addrAbsolute()),
 
       // LDX
-      [0xA2]: () => this.ldx(this.addrImmediate()),
-      [0xA6]: () => this.ldx(this.read(this.addrZeroPage())),
-      [0xAE]: () => this.ldx(this.read(this.addrAbsolute())),
+      [0xA2]: () => this.regX = this.addrImmediate(),
+      [0xA6]: () => this.regX = this.read(this.addrZeroPage()),
+      [0xAE]: () => this.regX = this.read(this.addrAbsolute()),
+
+      // AND
+      [0x29]: () => this.and(this.addrImmediate()),
 
       // TXS
       [0x9A]: () => this.regSP = this.regX,
+
+      // Branch instructions
+      [0x10]: () => this.branch((this.regSW & SW_FLAG_NEGATIVE) === 0),
+      [0xD0]: () => this.branch((this.regSW & SW_FLAG_ZERO) === 0),
     };
   }
 
@@ -97,7 +133,7 @@ export class Cpu {
     if (this.opCodeMap[opcode]) {
       this.opCodeMap[opcode]();
     } else {
-      console.log('unknown opcode: ', opcode.toString(16));
+      console.log('unknown opcode: ', (this.regPC - 1).toString(16), opcode.toString(16));
     }
   }
 
@@ -120,6 +156,11 @@ export class Cpu {
     const lowByte = this.readUint8();
     const highByte = this.readUint8();
     return (highByte << 8) | lowByte;
+  }
+
+  private toInt8(value: number) {
+    const byte = (value >>> 0) & MAX_BYTE;
+    return byte >= 0x80 ? byte - 0x100 : byte;
   }
 
   private setFlag(flag: ProcessorFlag, value: number) {
@@ -152,12 +193,12 @@ export class Cpu {
     return this.readUint16();
   }
 
-  // instructions
-
-  private lda(value: number) {
-    this.instructionCounter++;
-    this.regA = value;
+  private setRegisterFlags(value) {
+    this.setFlag(SW_FLAG_NEGATIVE, (value & 0x80) ? 1 : 0);
+    this.setFlag(SW_FLAG_ZERO, !value ? 1 : 0);
   }
+
+  // instructions
 
   private sta(address: number) {
     this.instructionCounter++;
@@ -166,9 +207,16 @@ export class Cpu {
     this.bus.setBusValue(this.regA);
   }
 
-  private ldx(value: number) {
-    this.instructionCounter++;
-    this.regX = value;
+  private and(value: number) {
+    this.regA = this.regA & value;
+  }
+
+  private branch(shouldBranch: boolean) {
+    const offset = this.readUint8();
+    if (shouldBranch) {
+      this.instructionCounter++;
+      this.regPC = this.regPC + this.toInt8(offset);
+    }
   }
 
 }
