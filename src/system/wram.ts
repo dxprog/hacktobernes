cba0512c5d7d8a8a5d9320f09cece1c18fca9dcc
Matt Hackmann <matt.hackmann@gmail.com>
2025-10-13 11:53:17 -0700

    debug mode for ram

--- src/system/wram.ts
+++ src/system/wram.ts
@@ -6,20 +6,29 @@ const ADDRESS_MASK = 0x7ff;
 
 export class Wram extends Chip {
   private ram: number[];
+  private debug: boolean;
 
-  constructor() {
+  constructor(debug: boolean = false) {
     super();
 
     // zero out the ram. maybe someday I'll fill it with a starting pattern
     // like an actual ram chip...
     this.ram = new Array(WRAM_SIZE).fill(0);
+    this.debug = debug;
   }
 
   read(address: number) {
-    return this.ram[address & ADDRESS_MASK];
+    const value = this.ram[address & ADDRESS_MASK];
+    if (this.debug) {
+      console.log('RAM read: $', address.toString(16), value.toString(16));
+    }
+    return value;
   }
 
   write(address: number, value: number) {
+    if (this.debug) {
+      console.log('RAM write: $', address.toString(16), value.toString(16));
+    }
     this.ram[address & ADDRESS_MASK] = value;
   }
 }
